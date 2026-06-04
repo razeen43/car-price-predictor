21e1155a4d45297d93fda4fd7edcbbc1abf1cae1
razeen43 <razc571@gmail.com>
2026-06-05 00:58:01 +0530

    Debug: add exception logging to index route

--- application.py
+++ application.py
@@ -15,19 +15,26 @@ model = pickle.load(open('LinearRegressionModel.pkl', 'rb'))
 car = pd.read_csv('Cleaned Car.csv')
 
 
+import traceback
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    companies = sorted(car['company'].unique())
-    car_models = sorted(car['name'].unique())
-    year = sorted(car['year'].unique(), reverse=True)
-    fuel_type = car['fuel_type'].unique()
+    try:
+        companies = sorted(car['company'].unique())
+        car_models = sorted(car['name'].unique())
+        year = sorted(car['year'].unique(), reverse=True)
+        fuel_type = car['fuel_type'].unique()
 
-    companies.insert(0, 'Select Company')
-    return render_template('index.html',
-                           companies=companies,
-                           car_models=car_models,
-                           years=year,
-                           fuel_types=fuel_type)
+        companies.insert(0, 'Select Company')
+        return render_template('index.html',
+                               companies=companies,
+                               car_models=car_models,
+                               years=year,
+                               fuel_types=fuel_type)
+    except Exception as e:
+        tb = traceback.format_exc()
+        return f"<pre>Error during rendering index page:\n{tb}</pre>", 500
+
 
 
 @app.route('/get_models', methods=['POST'])
